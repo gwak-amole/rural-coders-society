6e6f15512471bbed2a41b516e85b57c22c498f1a
gwak-amole <gwaksophie@gmail.com>
2025-12-13 15:30:32 -0800

    added more necessary pages and created dropdown bars in the navbar. aligned content properly.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 window.addEventListener("scroll", () => {
     const header = document.querySelector(".header");
     const navLinks = document.querySelectorAll(".nav-a");
+    const join = document.getElementById("#join-us-button")
     if (window.scrollY > 50) {
         header.classList.add("scrolled");
         navLinks.forEach(link => link.classList.add("scrolled"));
+        join.classList.add("scrolled");
     } else {
         header.classList.remove("scrolled");
         navLinks.forEach(link => link.classList.remove("scrolled"));
+        join.classList.add("scrolled");
     }
 });
