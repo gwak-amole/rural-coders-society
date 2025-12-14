623c3dd74658a67e7d53e65f0be262b4c69d197d
gwak-amole <gwaksophie@gmail.com>
2025-12-13 16:20:46 -0800

    added a hamburger for the navbar for when the window gets too narrow

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", () => {
     const header = document.querySelector(".header");
     const navLinks = document.querySelectorAll(".nav-a");
-    const join = document.getElementById("#join-us-button")
+    const join = document.getElementById("join-us-button")
     if (window.scrollY > 50) {
         header.classList.add("scrolled");
         navLinks.forEach(link => link.classList.add("scrolled"));
@@ -10,5 +10,20 @@ window.addEventListener("scroll", () => {
         header.classList.remove("scrolled");
         navLinks.forEach(link => link.classList.remove("scrolled"));
         join.classList.add("scrolled");
+    }
+});
+
+const hamburger = document.getElementById("hamburger");
+const sidebar = document.getElementById("sidebar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    sidebar.classList.toggle("open");
+})
+
+document.addEventListener("click", (e) => {
+    if (!sidebar.contains(e.target) && !hamburger.contains(e.target) && sidebar.classList.contains("open"))  {
+        sidebar.classList.remove("open");
+        hamburger.classList.toggle("active");
     }
 });
